11af66262f75730a3306accefd1a22f3a05e5e55
AdaskoKwi <adamnowakhomm@gmail.com>
2025-04-19 14:05:07 +0200

    Fixed product amount in cart updating to only 2.

--- src/main/java/org/example/ecommerce/repository/CartItemRepository.java
+++ src/main/java/org/example/ecommerce/repository/CartItemRepository.java
@@ -1,7 +1,11 @@
 package org.example.ecommerce.repository;
 
 import org.example.ecommerce.model.CartItem;
+import org.example.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
+    boolean existsByProductId(int id);
+
+    CartItem findByProduct(Product product);
 }
